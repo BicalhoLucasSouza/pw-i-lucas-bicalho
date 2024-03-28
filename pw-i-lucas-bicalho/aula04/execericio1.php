f4e3cc43a59a63541480d066da5b4454cec8a5f9
Lucas Bicalho <lucasbicalho20@gmail.com>
2024-03-28 20:30:03 -0300

    Aula functions

---
+++ pw-i-lucas-bicalho/aula04/execericio1.php
@@ -0,0 +1,9 @@
+<?php
+    function soma($x, $y) {
+        $s = $x + $y;
+        return $s;
+    }
+
+    echo "2 + 2 = " . soma(11, 11);
+
+?>
---
+++ pw-i-lucas-bicalho/aula04/functions.php
@@ -0,0 +1,14 @@
+<?php
+    function myMessage(){
+        echo "Hello porra!";
+    }
+
+    myMessage();
+
+    function familyName ($fname){
+        echo "$fname Bicalho.<br>";
+    }
+
+    familyName("lucas");
+    familyName("carla");
+?>
