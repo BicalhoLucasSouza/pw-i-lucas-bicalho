5d6ea29cf1d800abddac6b03c134c57c663c233f
Lucas Bicalho <lucasbicalho20@gmail.com>
2024-06-06 22:27:00 -0300

    aula 9

--- agenda_contatos/index.php
+++ agenda_contatos/index.php
@@ -1,6 +1,5 @@
 <?php include 'includes/header.php'; ?>
 
-<h1>Bem-vindo à Agenda de Contatos</h1>
-<p>Selecione um contato para visualizar.</p>
+<h1>Bem-vindo à Polen Planer</h1>
 
 <?php include 'includes/footer.php'; ?>
