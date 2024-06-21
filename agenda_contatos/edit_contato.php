b319976547fe12e683c14cd608332bafe1d4f1a1
lucas Bicalho <lucasbicalho20@gmail.com>
2024-06-21 20:45:18 -0300

    modificações lucas

--- agenda_contatos/edit_contato.php
+++ agenda_contatos/edit_contato.php
@@ -14,34 +14,46 @@ $contato = $stmt->fetch(PDO::FETCH_ASSOC);
     <head>
     <link rel="stylesheet" href="../css/edit.css"> 
     </head>
+    
         <body>
-        <fieldset class="fieldset" method="post" action="atualizar_dados.php">
-            <input class="input1" type="hidden" name="id" value="<?php echo $contato['id']; ?>">
-            <label class="label1">Nome:</label>
-            <input type="text" name="nome" value="<?php echo ($contato['nome']); ?>">
-            <br>
-            <label class="label1">Telefone:</label>
-            <input class="input1" type="text" name="telefone" value="<?php echo ($contato['telefone']); ?>">
-            <br>
-            <label class="label1">Profissão:</label>
-            <input class="input1" type="text" name="profissao" value="<?php echo ($contato['profissao']); ?>">
-            <br>
-            <label class="label1">E-mail:</label>
-            <input class="input1" type="text" name="email" value="<?php echo ($contato['email']); ?>">
-            <br>
-            <label class="label1">Rua:</label>
-            <input class="input1" type="text" name="rua" value="<?php echo ($contato['rua']); ?>">
-            <br>
-            <label class="label1">Número Rua:</label>
-            <input class="input1" type="text" name="numero" value="<?php echo ($contato['numero']); ?>">
-            <br>
-            <label class="label1">Cidade:</label>
-            <input class="input1" type="text" name="cidade" value="<?php echo ($contato['cidade']); ?>">
-            <br>
-            <label class="label1">Estado:</label>
-            <input class="input1" type="text" name="estado" value="<?php echo ($contato['estado']); ?>">
-            <br>
-            <input type="submit" value="Salvar">
-        </fieldset>
+        <script>
+    function exibirMensagem() {
+        alert("Informações salvas com sucesso!");
+    }
+</script>
+        <form action="atualizar_dados.php" method="post">
+    <fieldset class="fieldset">
+        <input type="hidden" name="id" value="<?php echo $contato['id']; ?>">
+        <label class="label1">Nome:</label>
+        <input type="text" name="nome" value="<?php echo ($contato['nome']); ?>"><br>
+        <br>
+        <label class="label1">Telefone:</label>
+        <input class="input1" type="text" name="telefone" value="<?php echo ($contato['telefone']); ?>"><br>
+        <br>
+        <label class="label1">Profissão:</label>
+        <input class="input1" type="text" name="profissao" value="<?php echo ($contato['profissao']); ?>"><br>
+        <br>
+        <label class="label1">E-mail:</label>
+        <input class="input1" type="text" name="email" value="<?php echo ($contato['email']); ?>"><br>
+        <br>
+        <label class="label1">Rua:</label>
+        <input class="input1" type="text" name="rua" value="<?php echo ($contato['rua']); ?>"><br>
+        <br>
+        <label class="label1">Número Rua:</label>
+        <input class="input1" type="text" name="numero" value="<?php echo ($contato['numero']); ?>"><br>
+        <br>
+        <label class="label1">Cidade:</label>
+        <input class="input1" type="text" name="cidade" value="<?php echo ($contato['cidade']); ?>"><br>
+        <br>
+        <label class="label1">Estado:</label>
+        <input class="input1" type="text" name="estado" value="<?php echo ($contato['estado']); ?>"><br>
+        <br>
+        <input type="submit" value="Salvar" onclick="exibirMensagem()">
+
+
+
+    </fieldset>
+</form>
+
 </body>
 </html>
