f2c0708447416e84374dec7294246ed79ca62587
Lucas Bicalho <lucasbicalho20@gmail.com>
2024-06-20 21:06:37 -0300

    Atualização Lucas

---
+++ agenda_contatos/deletar_contato.php
@@ -0,0 +1,21 @@
+<?php
+include 'config/database.php';
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    // Consulta para excluir o contato
+    $sql = "DELETE FROM contatos WHERE id = :id";
+    
+    try {
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([':id' => $id]);
+
+        // Redireciona para a página de listagem de contatos após a exclusão
+        header("Location: list_contatos.php");
+        exit();
+    } catch (PDOException $e) {
+        echo "Erro: " . $e->getMessage();
+    }
+}
+?>
